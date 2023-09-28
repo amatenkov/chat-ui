fe0200c93b27378da3e71b4b2e61725bfb707ad3
Матенков Андрей <amatenkov@ntrlab.ai>
2023-09-28 20:45:03 +0300

    build fix

--- src/lib/server/websearch/sentenceSimilarity.ts
+++ src/lib/server/websearch/sentenceSimilarity.ts
@@ -1,7 +1,6 @@
 import type { Tensor } from "@xenova/transformers";
 import { pipeline, dot } from "@xenova/transformers";
 //load Candle Bert Module wasm module
-import init, { Model } from "./build/m.js";
 
 // see here: https://github.com/nmslib/hnswlib/blob/359b2ba87358224963986f709e593d799064ace6/README.md?plain=1#L34
 function innerProduct(tensor1: Tensor, tensor2: Tensor) {
